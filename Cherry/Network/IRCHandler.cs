40098115004c2f731cf1c6675e281e7e1eb12ee0
인테그럴 <jmh121463@gmail.com>
2018-07-18 20:53:23 +0900

    네트워크 기능의 디렉토리 구조를 만들어 보았다

---
+++ Cherry/Network/IRCHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Net.NetworkInformation;
+
+namespace Cherry.Network
+{
+    class IRCHandler : NetworkHandler
+    {
+    }
+}
---
+++ Cherry/Network/NetworkHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace Cherry.Network
+{
+    class NetworkHandler
+    {
+        NetworkStream networkStream;
+        IPAddress iPAddress;
+        bool isConnectionEstablished;
+
+        public NetworkHandler()
+        {
+
+        }
+
+        public void Connect()
+        {
+
+        }
+
+        public void Write()
+        {
+
+        }
+
+        public string Read()
+        {
+
+        }
+    }
+}
