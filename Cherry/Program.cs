3c0eb92108fd5ddd5fdd5c41b9d9aa44d693e5f4
MinHyeok Jeon <jmh121463@gmail.com>
2019-04-12 02:01:02 +0900

    Whole Cleanup

--- Cherry/Program.cs
+++ Cherry/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using Cherry.Network;
-using Cherry.Service;
 
 namespace Cherry
 {
@@ -8,12 +6,7 @@ namespace Cherry
     {
         static void Main(string[] args)
         {
-            if(args.Length == 0)
-            {
-                Console.WriteLine("Please Input IRC server's url with port number at arguments");
-                return;
-            }
-            ServiceManager serviceManager = new ServiceManager(new IRCHandler(new NetworkHandler(args[0]), "cherryT","CherryT"));
+            Console.WriteLine("CleanUp");
         }
     }
 }
